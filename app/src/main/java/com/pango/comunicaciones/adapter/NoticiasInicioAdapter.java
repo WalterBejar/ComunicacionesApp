cc54fd81130a1a874e874c4f3ce6347a13efb90b
WalterBCH <wbch_15@hotmail.com>
2017-09-25 15:06:56 -0500

    Archivos Noticias Inicio

---
+++ app/src/main/java/com/pango/comunicaciones/adapter/NoticiasInicioAdapter.java
@@ -0,0 +1,9 @@
+package com.pango.comunicaciones.adapter;
+
+/**
+ * Created by WalterBCH on 25/9/2017.
+ */
+
+public class NoticiasInicioAdapter {
+
+}
---
+++ app/src/main/java/com/pango/comunicaciones/controller/NoticiasInicioController.java
@@ -0,0 +1,16 @@
+package com.pango.comunicaciones.controller;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.util.Log;
+
+/**
+ * Created by WalterBCH on 25/9/2017.
+ */
+
+public class NoticiasInicioController extends AsyncTask<String,Void,Void> {
+    @Override
+    protected Void doInBackground(String... params) {
+        return null;
+    }
+}
