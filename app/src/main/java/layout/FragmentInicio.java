d927adc3d921fe3b591f9d1318f59b766213ff00
Hubert <d.palosoto@outlook.com>
2017-09-25 13:36:57 -0500

    test 01

--- app/src/main/java/layout/FragmentInicio.java
+++ app/src/main/java/layout/FragmentInicio.java
@@ -27,6 +27,7 @@ public class FragmentInicio extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
+    // walter necesita ladrillo
     private String mParam1;
     private String mParam2;
 
