982fbcc4eefcef04a0224d8b1c370a00f774a821
WalterBCH <wbch_15@hotmail.com>
2017-09-25 16:59:52 -0500

    Probando fragments en inicio

--- app/src/main/java/com/pango/comunicaciones/adapter/NoticiasInicioAdapter.java
+++ app/src/main/java/com/pango/comunicaciones/adapter/NoticiasInicioAdapter.java
@@ -1,9 +1,67 @@
 package com.pango.comunicaciones.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.pango.comunicaciones.R;
+import com.pango.comunicaciones.model.NoticiasModel;
+
+import java.util.List;
+
 /**
  * Created by WalterBCH on 25/9/2017.
  */
 
-public class NoticiasInicioAdapter {
+public class NoticiasInicioAdapter extends ArrayAdapter<NoticiasModel> {
 
+    private List<NoticiasModel> data;
+    private Context context;
+
+    public NoticiasInicioAdapter(Context context, List<NoticiasModel> data) {
+        super(context, R.layout.item_carrete, data);
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View rowView=inflater.inflate(R.layout.item_carrete, null,true);
+
+        TextView nFecha = (TextView)  rowView.findViewById(R.id.txt_fecha);
+        TextView nTitulo = (TextView)  rowView.findViewById(R.id.txt_titulo);
+        ImageView nImagen = (ImageView)  rowView.findViewById(R.id.img_carrete);
+
+        final String tempFecha = data.get(position).getFecha();
+        final String tempTitulo = data.get(position).getTitulo();
+
+        nFecha.setText(tempFecha);
+        nTitulo.setText(tempTitulo);
+
+        int ds = data.get(position).getFiledata().size();
+
+        /*if(ds==0) {
+        }else {*/
+            Glide.with(context)
+                    .load("https://app.antapaccay.com.pe/Proportal/SCOM_Service/api/media/GetImagen/4056/CUMPLEAÑOS.jpg".replaceAll("\\s", "%20"))
+                    .into(nImagen);
+        //}
+
+        nImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Se hizo click en la imagen", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rowView;
+    }
 }
--- app/src/main/java/com/pango/comunicaciones/controller/NoticiasInicioController.java
+++ app/src/main/java/com/pango/comunicaciones/controller/NoticiasInicioController.java
@@ -1,16 +1,63 @@
 package com.pango.comunicaciones.controller;
 
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.pango.comunicaciones.adapter.NoticiasInicioAdapter;
+import com.pango.comunicaciones.R;
+import com.pango.comunicaciones.model.NoticiasModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import layout.FragmentInicio;
 
 /**
  * Created by WalterBCH on 25/9/2017.
  */
 
 public class NoticiasInicioController extends AsyncTask<String,Void,Void> {
+
+    View v;
+    FragmentInicio fragmentInicio;
+    ListView listViewNoticias;
+    List<NoticiasModel> listaNoticias = new ArrayList<NoticiasModel>();
+
+    public NoticiasInicioController(View v, FragmentInicio Frag){
+        this.v = v;
+        fragmentInicio = Frag;
+        listViewNoticias = (ListView) v.findViewById(R.id.listViewNoticias);
+    }
+
     @Override
     protected Void doInBackground(String... params) {
         return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        Toast.makeText(v.getContext(), "Se cargaron los videos", Toast.LENGTH_SHORT).show();
+
+        for (int i = 0; i < 10; i++)
+        {
+            NoticiasModel noticia = new NoticiasModel();
+            noticia.setTitulo("Este es el titulo de la noticia Nro: " + (i+1));
+            noticia.setFecha("21-09-2017");
+            listaNoticias.add(noticia);
+        }
+
+    }
+    @Override
+    protected  void onPostExecute(Void result){
+        try {
+            NoticiasInicioAdapter adapter = new NoticiasInicioAdapter(v.getContext(), listaNoticias);
+            listViewNoticias.setAdapter(adapter);
+        }catch (Exception ex){
+            Log.w("Error",ex);
+        }
     }
 }
